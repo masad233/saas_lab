0d19ae4a46820a46cab6fb157513653d30b75a7a
ahasan <asad263@hotmail.com>
2022-07-28 16:57:27 -0400

    update transit

--- main.tf
+++ main.tf
@@ -2,4 +2,14 @@ module "mc-transit" {
   source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
   version = "2.1.6"
   # insert the 3 required variables here
+  cloud = "AWS"
+  region = "us-east-1"
+  cidr = "10.100.10.0/23"
+  account = "AWS"
+  enable_segmentation = true
+  enable_transit_firenet = true
+  gw_name = "AWS-Transit"
+  ha_gw = false
+  local_as_number = 65100
+  name = "vpc-saas-transit"
 }
