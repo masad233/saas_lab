7d197f5f48719d3f546dcc3f2aeb2ed0c4dc4d5b
ahasan <asad263@hotmail.com>
2022-08-01 15:14:44 -0400

    added customer site with vm

--- providers.tf
+++ providers.tf
@@ -1,8 +1,15 @@
 terraform {
   required_providers {
     aviatrix = {
-      source = "AviatrixSystems/aviatrix"
+      source  = "AviatrixSystems/aviatrix"
       version = "2.22.2"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
+}
+provider "aws" {
+  region = "us-east-2"
 }
